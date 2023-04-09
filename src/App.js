a39e08b29d7acb022b5e617071a269c2f53855ae
Thomas Béguin <thom.beguin@gmail.com>
2023-04-09 10:56:58 +0200

    first React exo

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello</p>
+      <Test name="toto"/>
     </div>
   );
 }
 
+function Test(props) {
+  return (
+    <div>
+      <p>{props.name}</p>
+    </div>
+  )
+}
 export default App;

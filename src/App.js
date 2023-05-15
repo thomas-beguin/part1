24b05b2c7ce84ea42c62b71bcfe7a32a9ab96114
Thomas Béguin <thom.beguin@gmail.com>
2023-05-15 18:36:47 +0200

    all exercices part1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import userEvent from '@testing-library/user-event'
 import { useState, useSyncExternalStore } from 'react'
 
 const Display = ({counter}) => <div>{counter}</div>
+const Tr = ({title, value}) => <tr><th>{title}</th><th>{value}</th></tr>
+const Anecdote = (props) => <div>{props.anecdote}</div>
 
 const Button = (props) => <div><button onClick={props.method}>{props.text}</button></div>
 
@@ -15,12 +17,16 @@ const Statistics = (props) => {
   } else {
     return (
       <div>
-        <Display counter={`Good ${props.good}`}/>
-        <Display counter={`Neutral ${props.neutral}`}/>
-        <Display counter={`Bad ${props.bad}`}/>
+        <table>
+          <tbody>
+            <Tr title={'Good'} value = {props.good}/>
+            <Tr title={'Neutral'} value = {props.neutral}/>
+            <Tr title={'Bad'} value = {props.bad}/>
 
-        <span><Display counter={`Average ${(props.good - props.bad) / (props.good + props.bad + props.neutral)}`}/></span>
-        <span><Display counter={`Positive ${(props.good / (props.good + props.neutral + props.bad)) * 100}%`}/></span>
+            <Tr title={'Average'} value = {(props.good - props.bad) / (props.good + props.bad + props.neutral)}/>
+            <Tr title={'Positive'} value = {`${(props.good / (props.good + props.neutral + props.bad)) * 100}%`}/>
+          </tbody>
+        </table>
       </div>
     )
 
@@ -31,11 +37,38 @@ const App = (props) => {
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
   const [ bad, setBad] = useState(0)
-  console.log(bad, good, neutral)
+  const [ selected, setSelected] = useState(0)
+
+  const [anecdotes, setAnecdotes] = useState({
+    'If it hurts, do it more often.': 0,
+    'Adding manpower to a late software project makes it later!': 0,
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.': 0,
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.': 0,
+    'Premature optimization is the root of all evil.': 0,
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.': 0,
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.': 0,
+    'The only way to go fast, is to go well.':0
+  })
+  // const [ votes, setVotes] = useState(Object.values(anecdotes))
 
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
   const increaseBad = () => setBad(bad + 1)
+  const randomAnecdote = () => {
+    let rand = Math.floor(Math.random()*Object.keys(anecdotes).length)
+    setSelected(rand)
+  }
+  const [LargestAnectode, setLargest] = useState("")
+
+  let copy = {...anecdotes}
+
+  const likeAnecdote = () => {
+    copy[Object.keys(copy)[selected]]++
+    setAnecdotes(copy)
+    if (copy[Object.keys(copy)[selected]] >= Math.max(...Object.values(copy))) {
+      setLargest(Object.keys(copy)[selected])
+    }
+  }
 
   return (
     <div>
@@ -44,6 +77,12 @@ const App = (props) => {
       <Button method={increaseBad} text="Bad" />
       <h1>Statistics</h1>
       <Statistics good={good} bad={bad} neutral={neutral} />
+      <Button method={randomAnecdote} text='Next one'/>
+      <Anecdote anecdote={Object.keys(anecdotes)[selected]} />
+      <Anecdote anecdote={Object.values(copy)[selected]} />
+      <Button method={likeAnecdote} text="Like it"/>
+      <h1>Anecdote with the largest vote</h1>
+      <Anecdote anecdote={LargestAnectode} />
     </div>
   );
 }

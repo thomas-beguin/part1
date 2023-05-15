6629a13a20946f6fae56617c8b66da1c8731870d
Thomas Béguin <thom.beguin@gmail.com>
2023-05-15 17:09:04 +0200

    exercice 1.8

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ const Display = ({counter}) => <div>{counter}</div>
 
 const Button = (props) => <div><button onClick={props.method}>{props.text}</button></div>
 
+const Statistics = (props) => {
+  if (props.good === 0 && props.bad === 0 && props.neutral === 0) {
+    return (
+      <div>
+        <Display counter={'No statistics for now'} />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Display counter={`Good ${props.good}`}/>
+        <Display counter={`Neutral ${props.neutral}`}/>
+        <Display counter={`Bad ${props.bad}`}/>
+
+        <span><Display counter={`Average ${(props.good - props.bad) / (props.good + props.bad + props.neutral)}`}/></span>
+        <span><Display counter={`Positive ${(props.good / (props.good + props.neutral + props.bad)) * 100}%`}/></span>
+      </div>
+    )
+
+  }
+}
+
 const App = (props) => {
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
   const [ bad, setBad] = useState(0)
+  console.log(bad, good, neutral)
 
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
@@ -20,12 +43,7 @@ const App = (props) => {
       <Button method={increaseNeutral} text="Neutral" />
       <Button method={increaseBad} text="Bad" />
       <h1>Statistics</h1>
-      <Display counter={`Good ${good}`}/>
-      <Display counter={`Neutral ${neutral}`}/>
-      <Display counter={`Bad ${bad}`}/>
-
-      <span><Display counter={`Average ${(good - bad) / (good + bad + neutral)}`}/></span>
-      <span><Display counter={`Positive ${(good / (good + neutral + bad)) * 100}%`}/></span>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 }
